0ab8bdf943e3ca2c255c7a29d4b85d0df35d4dbf
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2017-03-04 17:54:01 +0700

    initial lexer and token set

---
+++ lexer/lexer_test.go
@@ -0,0 +1,72 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/phoorichet/monkey/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+	  x + y;
+};
+
+let result = add(five, ten);`
+
+	tests := []struct {
+		wType    token.TokenType
+		wLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+	}
+
+	l := New(input)
+	for i, test := range tests {
+		tok := l.NextToken()
+		if tok.Type != test.wType {
+			t.Errorf("#%d: got '%s', want '%s'", i+1, tok.Type, test.wType)
+		}
+
+		if tok.Literal != test.wLiteral {
+			t.Errorf("#%d: got '%s', want %s", i+1, tok.Literal, test.wLiteral)
+		}
+	}
+}
